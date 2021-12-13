727ff104877f3e1e196b2ab8942032640db4a4eb
Michal Kopulety <michal.kopulety@sap.com>
2021-12-13 18:35:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const puppeteer = require('./lib/puppeteer');
+const storeResults = require('./lib/storeResult');
+
+const sp100 = require('./lib/sp100');
+const sp500 = require('./lib/sp500');
+
+const getCompanies = async (universe) => {
+    const companies = await puppeteer(universe.url, universe.selector, universe.parser);
+    await storeResults(universe.path, companies);
+}
+
+// const russell3000 = require('./russell3000');
+(async() => {
+    [sp100, sp500].forEach(async (universe) => {
+        await getCompanies(universe);
+    })
+})();
